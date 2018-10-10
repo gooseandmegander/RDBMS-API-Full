0b4f650a43ba450ae91a041ed1771cc6590b9f0d
gooseandmegander <meganawilliamson@gmail.com>
2018-10-10 12:05:25 -0700

    setting up project dded package.json and database dir with two tables

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/lambda.sqlite3',
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    useNullAsDefault: true,
+  },
+};
